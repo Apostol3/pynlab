74a9bf5604d8f58cb07d5bc15e01529e1aeaeb9a
Apostol3 <apostol3.mv@yandex.ru>
2015-11-22 14:38:09 +0300

    add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='pynlab',
+    version='0.1.0',
+    packages=[''],
+    url='',
+    install_requires=['pywin32>=219'],
+    license='MIT',
+    author='Kirill Dudkin',
+    author_email='apostol3.mv@yandex.ru',
+    description=''
+)
